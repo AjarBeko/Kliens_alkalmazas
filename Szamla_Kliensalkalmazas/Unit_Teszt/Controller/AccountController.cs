28bb1f86c316f9520105a383e11f558ccd415c23
Bekő Osgyáni <bekoosgyani@gmail.com>
2025-04-26 13:38:45 +0200

    mi a gyászért nem vagy jó????

---
+++ Szamla_Kliensalkalmazas/Unit_Teszt/Controller/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Unit_Teszt.Controller
+{
+    public class AccountController
+    {
+        public bool ValidateUserName(string username)
+        {
+            if (username == null) return false;
+            return Regex.IsMatch(username, "^admin$");
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            if (password == null) return false;
+            return Regex.IsMatch(password, "^KrumpliFozelek2025$");
+        }
+    }
+}
